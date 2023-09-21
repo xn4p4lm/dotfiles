74c8a9db6949d6d33f0eea44f2156848db5e6321
Diana <xn4p4lm@github.com>
2023-09-21 23:51:50 +0000

    update the paths to dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env fish
 
-# Define variables
-set -x FISH_CONFIG_DIR ~/.config/fish
-set -x DOTFILES_REPO "https://github.com/xn4p4lm/dotfiles.git"
-set -x WORKSPACES_DIR /workspaces
-set -x DOTFILES_DIR $WORKSPACES_DIR/dotfiles
-set -x Currenenv
+set -x DOTFILE_DIR /workspaces/.codespaces/.persistedshare/dotfiles
 
 # Define fish configs folder
-set -x FISH_CONFIG_DIR $DOTFILES_DIR/fish
+set -x FISH_CONFIG_DIR $DOTFILE_DIR/fish
 
 # Define git configs folder
-set -x GIT_CONFIG_DIR $DOTFILES_DIR/git
+set -x GIT_CONFIG_DIR $DOTFILE_DIR/git
 
 function source_files -d "Source fish and git config files"
     # install fish plugins
@@ -24,23 +19,4 @@ function source_files -d "Source fish and git config files"
     source $GIT_CONFIG_DIR/configure_git.fish
 end
 
-# clone dotfiles repo into workspace if it doesn't exist
-if not test -d $DOTFILES_DIR
-    git clone $DOTFILES_REPO $DOTFILES_DIR
-
-    # if branch variable is set, checkout that branch
-    if test -n "$DOTFILES_BRANCH"
-        git -C $DOTFILES_DIR checkout $DOTFILES_BRANCH
-    end
-
-    source_files
-
-else
-
-    source_files
-    
-end
-
-
-
-
+source_files
